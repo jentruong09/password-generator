92284eeb5a6165cd1742462a35a444fce859fcb0
Jen Truong <jennifertruong09@gmail.com>
2022-01-20 20:45:03 -0500

    added an if statement so that the user will know if  they did not pick any characters and added empty if statements for what the user picks (will update)

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var specialCharacters = " !&#$%)(*+?\|:;_-<>,.^";
 var passwordLength = " ";
 
 
+
 function generatePassword() {
    var passwordLength = (prompt("Please select a number from 8 to 128 for password length."));
    
@@ -14,17 +15,32 @@ function generatePassword() {
     alert("Incorrect input. Please select a number from 8 to 128 for password length.");
    
    } else if (passwordLength >= 8 && passwordLength <= 128) {
-    var confirmLowerCase = confirm("Would you like lower case letters in your password?");
-    var confirmUpperCase = confirm("Would you like upper case letters in your password?");
-    var confirmNumbers = confirm("Would you like numbers in your password?");
-    var confirmSpecialCharacters = confirm("Would you like special characters in you password?")
-   }
-
-
+    var confirmLowerCase = confirm("Would you like lower case letters in your password? Click 'OK' for yes. Click 'Cancel' for no.");
+    var confirmUpperCase = confirm("Would you like upper case letters in your password? Click 'OK' for yes. Click 'Cancel' for no.");
+    var confirmNumbers = confirm("Would you like numbers in your password? Click 'OK' for yes. Click 'Cancel' for no.");
+    var confirmSpecialCharacters = confirm("Would you like special characters in you password? Click 'OK' for yes. Click 'Cancel' for no.")
+    // Confirming that user selects at least one of the character types
+   }if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialCharacters === false) {
+      alert("You must pick at least one character type.");
+    if (confirmLowerCase == true) {
+      
+    }
+    if (confirmUpperCase == true) {
+      
+    }
+    if (confirmNumbers == true) {
+      
+    }
+    if (confirmSpecialCharacters) {
+      
+    }
+  } 
   }
 
+  
 
 
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
