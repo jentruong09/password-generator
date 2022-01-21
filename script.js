bcc076e6307a033db98b8c69e479d21b510afdd4
Jen Truong <jennifertruong09@gmail.com>
2022-01-20 20:07:53 -0500

    added the else if statement to the js code and for user to confirm if they want anytype of characters

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// Assignment Code
+// Assignment Code: what each variable stores
 var generateBtn = document.querySelector("#generate");
 var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
 var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
 var specialCharacters = " !&#$%)(*+?\|:;_-<>,.^";
-var passwordLength = " "
+var passwordLength = " ";
+
 
 function generatePassword() {
    var passwordLength = (prompt("Please select a number from 8 to 128 for password length."));
    
    if (passwordLength < 8 || passwordLength > 128) {
     alert("Incorrect input. Please select a number from 8 to 128 for password length.");
-  } 
-  }
+   
+   } else if (passwordLength >= 8 && passwordLength <= 128) {
+    var confirmLowerCase = confirm("Would you like lower case letters in your password?");
+    var confirmUpperCase = confirm("Would you like upper case letters in your password?");
+    var confirmNumbers = confirm("Would you like numbers in your password?");
+    var confirmSpecialCharacters = confirm("Would you like special characters in you password?")
+   }
 
+
+  }
 
 
 
